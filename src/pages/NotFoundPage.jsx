98af0e2f733b3bf2acbb876fecb1e682c6e63d51
Kai1901 <truonggiakhanham@gmail.com>
2026-03-05 22:41:56 +0700

    Generate the HTML for the home page

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,14 @@
+import { Header } from "../components/Header";
+
+export function NotFoundPage () {
+    return (
+        <>
+            <title>404 (Not Found)</title>
+            <Header />
+
+            <p>
+                Khong tim thay gi!
+            </p>
+        </>
+    );
+}
