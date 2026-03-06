9fafafd0d1faafc451f65ba33292a9db41bbfe0e
Kai1901 <truonggiakhanham@gmail.com>
2026-03-06 15:36:38 +0700

    Convert home and checkout to async await

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,22 @@ function App() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/cart-items?expand=product')
-      .then((response) => {
-          setCart(response.data);
-      });
+    const fetchAppData = async () => {
+      const response = await axios.get('/api/cart-items?expand=product')
+      setCart(response.data);
+    };
+
+    fetchAppData();
   }, []);
 
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart}/>} />
-      <Route path="/checkout" element={<CheckoutPage cart={cart}/>} />
-      <Route path="/orders" element={<OrdersPage cart={cart}/>} />
-      <Route path="/tracking" element={ <TrackingPage />} />
-      <Route path="/404page" element={ <NotFoundPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="/orders" element={<OrdersPage cart={cart} />} />
+      <Route path="/tracking" element={<TrackingPage />} />
+      <Route path="/404page" element={<NotFoundPage />} />
     </Routes>
   )
 }
